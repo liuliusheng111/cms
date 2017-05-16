4d9150e4061be7ed2e44dc4a7bfca416f577a79f
liuls <123>
2017-05-16 09:25:42 +0800

    常量

---
+++ src/main/java/com/jflyfox/common/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.jflyfox.common.utils;
+
+/**
+ * 系统通用的一些常量
+ * Created by liuls on 17/5/16.
+ */
+public class Constants {
+    public static final String IMAGE_PRIFIX_URL = "http://ool6v2nck.bkt.clouddn.com/"; //图片上传到图片服务器路径
+}
